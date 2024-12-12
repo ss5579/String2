9ec4c37ea7ee2b8bebda9dfc3cbc42cf89f97d8c
ss5579 <130932847+ss5579@users.noreply.github.com>
2024-12-12 23:02:08 +0600

    Задание 2

---
+++ Main2.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class Main2 {
+    public static void main(String[] args) {
+        String stroke = "12345";
+        char symbol = stroke.charAt(0);
+        char symbol_1 = stroke.charAt(4);
+//        System.out.println(symbol);
+//        System.out.println(symbol_1);
+
+        String result = "" + symbol + symbol_1;
+        System.out.println(result);
+    }
+}
